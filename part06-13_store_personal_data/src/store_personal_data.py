ee56c96aec301c8f17a67ca9154637eacbb243ab
Holl4444 <holly.cello@gmail.com>
2025-08-27 20:04:38 +0100

    Solved up to part06-14

--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,18 @@
 # Write your solution here
+def store_personal_data(person: tuple[str, int, float]) -> None:
+    with open('people.csv', 'a') as people:
+        (name, age, height) = person
+        people.write(f'{name};{age};{height}\n')
+
+# def read_file(filename):
+#     with open(filename) as f:
+#         content = f.read()
+#         print(content)
+
+# def main():
+#     read_file('people.csv')
+#     store_personal_data(("Paul Paulson", 37, 175.5))
+#     read_file('people.csv')
+
+# if __name__ == '__main__':
+#     main()
