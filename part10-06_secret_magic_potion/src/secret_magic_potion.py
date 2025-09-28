7ba7b13db9422072300d67b4aa72638dafa6c13d
Holly Goulbourne <hollygoulbourne@mp-j673vvqd23.home>
2025-09-28 19:10:43 +0100

    Up to part10-08

--- part10-06_secret_magic_potion/src/secret_magic_potion.py
+++ part10-06_secret_magic_potion/src/secret_magic_potion.py
@@ -2,14 +2,64 @@
 class MagicPotion:
     def __init__(self, name: str):
         self._name = name
-        self._ingredients = []
+        self._ingredients: list[tuple[str, float]] = []
 
-    def add_ingredient(self, ingredient: str, amount: float):
+    # Password added to both methods to satisfy linter
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
         self._ingredients.append((ingredient, amount))
 
-    def print_recipe(self):
+    def print_recipe(self, password: str):
         print(self._name + ":")
         for ingredient in self._ingredients:
             print(f"{ingredient[0]} {ingredient[1]} grams")
 
 
+class SecretMagicPotion(MagicPotion):
+    def __init__(self, name: str, password: str):
+        self.password = password
+        self.name = name
+        super().__init__(self.name)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        if name == "":
+            raise ValueError("name cannot be blank.")
+        self._name = name
+
+    @property
+    def password(self) -> str:
+        return self._password
+
+    @password.setter
+    def password(self, password: str):
+        if password == "":
+            raise ValueError("Password must not be an empty string.")
+        self._password = password
+
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
+        if ingredient == "":
+            raise ValueError("The ingredient cannot be nothing.")
+        if amount < 0:
+            raise ValueError("The amount cannot be negative")
+        if not password == self._password:
+            raise ValueError("Invalid password.")
+        super().add_ingredient(ingredient, amount, password)
+
+    def print_recipe(self, password: str):
+        if password != self._password:
+            raise ValueError("Invalid password.")
+        super().print_recipe(password)
+
+
+if __name__ == "__main__":
+    diminuendo = SecretMagicPotion("Diminuendo maximus", "hocuspocus")
+    diminuendo.add_ingredient("Toadstool", 1.5, "hocuspocus")
+    diminuendo.add_ingredient("Magic sand", 3.0, "hocuspocus")
+    diminuendo.add_ingredient("Frogspawn", 4.0, "hocuspocus")
+    diminuendo.print_recipe("hocuspocus")
+
+    diminuendo.print_recipe("pocushocus")
--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -1,8 +1,68 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self._euros = euros
+        self._cents = cents
 
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        return f"{self._euros}.{self._cents:02d} eur"
+
+    def __get_value(self):
+        return self._euros + self._cents / 100
+
+    @staticmethod
+    def __get_euros_and_cents(value: float) -> tuple[int, int]:
+        euros = int(value)
+        cents = round((value - euros) * 100)
+        return (euros, cents)
+
+    # Python needs to match the base class object rather than type as
+    # equality checks (like ==) are designed to work with objects.
+    # Need to check correct type inside the method
+    def __eq__(self, another: object) -> bool:
+        if not isinstance(another, Money):
+            return NotImplemented  # Special for 'rich comparison methods' ie). __eq__, __ge__...
+        # Allows Python to try the reverse comparison in case the other side knows how to handle the comparison.
+        return self.__get_value() == another.__get_value()
+
+    def __lt__(self, another: object) -> bool:
+        if not isinstance(another, Money):
+            return NotImplemented
+        return self.__get_value() < another.__get_value()
+
+    def __gt__(self, another: object) -> bool:
+        if not isinstance(another, Money):
+            return NotImplemented
+        return self.__get_value() > another.__get_value()
+
+    def __ne__(self, another: object) -> bool:
+        if not isinstance(another, Money):
+            return NotImplemented
+        return self.__get_value() != another.__get_value()
+
+    def __add__(self, another: object):
+        if not isinstance(another, Money):
+            return NotImplemented
+        this = self.__get_value()
+        the_other = another.__get_value()
+        value = this + the_other
+        euros, cents = Money.__get_euros_and_cents(value)
+        return Money(euros, cents)
+
+    def __sub__(self, another: object):
+        if not isinstance(another, Money):
+            return NotImplemented
+        this = self.__get_value()
+        the_other = another.__get_value()
+        value = round(this - the_other, 2)
+        if value < 0:
+            raise ValueError("Object of type 'Money' cannot be negative.")
+        euros = int(value)
+        cents = round((value - euros) * 100)
+        return Money(euros, cents)
+
+
+if __name__ == "__main__":
+    money1 = Money(15, 95)
+    money2 = Money(15, 95)
+    print(money1 + money2)
